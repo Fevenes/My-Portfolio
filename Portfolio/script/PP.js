9f68cb40383ae6ad87e885f042a19feff480056f
Fevenes <fevaesa82@gmail.com>
2024-06-27 05:40:38 -0700

    Updated

--- Portfolio/script/PP.js
+++ Portfolio/script/PP.js
@@ -5,3 +5,19 @@ function toggleMenu() {
     icon.classList.toggle("open");
 
 }
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzIVtK35x3-02hsXiDBMMvq_7VP4AEHF6GAlcb8t-7C6Dtw8_6NO9zj6-T8zc9VTlxHeA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+      .catch(error => console.error('Error!', error.message))
+  })
